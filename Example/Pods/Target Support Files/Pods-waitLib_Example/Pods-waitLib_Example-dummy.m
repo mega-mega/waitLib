30b57743113b724d514d52c9e6bbe5bea05e4067
mega-mega <t.shibuya.uva@gmail.com>
2018-03-09 22:44:05 +0900

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-waitLib_Example/Pods-waitLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_waitLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_waitLib_Example
+@end
---
+++ Example/Pods/Target Support Files/waitLib/waitLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_waitLib : NSObject
+@end
+@implementation PodsDummy_waitLib
+@end
